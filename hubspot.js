20d42c170911e72bf7687475b9c7670cfda4e185
Élio Cró <elio@front.no>
2016-02-29 17:35:08 +0000

    Implemented hubspot request

--- hubspot.js
+++ hubspot.js
@@ -2,59 +2,69 @@
 
 const request = require('request');
 
+const endpoint = 'https://api.hubapi.com/contacts/v1';
+const apiKey = process.env.HS_KEY;
+const listId = process.env.HS_LIST;
+
+if(!apiKey || !listId) {
+  console.log('Env vars not set!');
+  process.exit(0);
+}
 
 
+function createContact (email, firstname, lastname) {
+  let data = {
+    properties: [{
+      property: 'email',
+      value: email
+    }, {
+      property: 'firstname',
+      value: firstname
+    }, {
+      property: 'lastname',
+      value: lastname
+    }]
+  };
 
-module.exports = {
-  createContact (email, firstname, lastname) {
-    let data = {
-      properties: [{
-        property: 'email',
-        value: email
-      }, {
-        property: 'firstname',
-        value: firstname
-      }, {
-        property: 'lastname',
-        value: lastname
-      }]
+  request({
+    method: 'POST',
+    url: endpoint + '/contact',
+    qs: {
+      hapikey: apiKey
+    },
+    json: true,
+    body: data
+  },
+  (err, res, body) => {
+    if(err) {
+      return console.log(err);
+    }
+    console.log(body);
+    console.log('----------------');
+
+    let data2 = {
+      vids: [ body.vid ]
     };
 
     request({
       method: 'POST',
-      url: endpoint + '/contact',
+      url: endpoint + '/lists/' + listId + '/add',
       qs: {
         hapikey: apiKey
       },
       json: true,
-      body: data
+      body: data2
     },
     (err, res, body) => {
       if(err) {
         return console.log(err);
       }
       console.log(body);
-      console.log('----------------');
-
-      let data2 = {
-        vids: [ body.vid ]
-      };
-
-      request({
-        method: 'POST',
-        url: endpoint + '/lists/' + listId + '/add',
-        qs: {
-          hapikey: apiKey
-        },
-        json: true,
-        body: data2
-      },
-      (err, res, body) => {
-        if(err) {
-          return console.log(err);
-        }
-        console.log(body);
-      });
     });
-  }
+  });
+}
+
+
+module.exports = {
+  createContact
 };
